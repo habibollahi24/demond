1bd6050ae85fb5f45477f05f25168c60a3d3152d
mohammad <habibollahi24@gmail.com>
2025-06-13 16:25:12 +0330

    directive use cache added and better performance

--- src/actions/action.currentuser.ts
+++ src/actions/action.currentuser.ts
@@ -1,30 +1,34 @@
 'use server';
 
 import { UserType } from '@/features/auth/type.auth';
-import { cache } from 'react';
+// import { cache } from 'react';
 
-const currentUser = cache(
-  async (token: string | undefined, restData: string) => {
-    console.log('cache apply');
+const currentUser = async (
+  token: string | undefined,
+  restData: string
+) => {
+  'use cache';
+  // console.log('cache apply');
 
-    if (!token || !restData) return { message: 'Error', user: null };
+  if (!token || !restData) return { message: 'Error', user: null };
 
-    const res = await fetch(
-      `https://randomuser.me/api/?results=1&nat=us&seed=${token}`
-    );
+  const res = await fetch(
+    `https://randomuser.me/api/?results=1&nat=us&seed=${token}`
+  );
 
-    if (!res.ok) return { message: 'Error', user: null };
-    const data = await res.json();
-    const user = data.results[0];
+  if (!res.ok) return { message: 'Error', user: null };
+  const data = await res.json();
+  const user = data.results[0];
 
-    return {
-      message: 'Ok',
-      user: {
-        ...user,
-        ...JSON.parse(String(restData)),
-      } as UserType,
-    };
-  }
-);
+  console.log('cache apply');
+
+  return {
+    message: 'Ok',
+    user: {
+      ...user,
+      ...JSON.parse(String(restData)),
+    } as UserType,
+  };
+};
 
 export default currentUser;
